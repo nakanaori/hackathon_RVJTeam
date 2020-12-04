a8420f6ae46013159b7c431ab4e327580a78e523
nakanaori <nakanaori@github.com>
2020-12-04 21:23:30 +0700

    firebase, blum jadi

--- app/src/main/java/com/u_binusportal/OTPGenerate.java
+++ app/src/main/java/com/u_binusportal/OTPGenerate.java
@@ -1,25 +1,52 @@
 package com.u_binusportal;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
 import com.chaos.view.PinView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.u_binusportal.activity.entriesActivities.entries.RegisterActivity;
+import com.u_binusportal.component.User;
 import com.u_binusportal.forTesting.UserTesting;
 import com.u_binusportal.handlers.FragmentHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class OTPGenerate extends AppCompatActivity {
 
     private PinView pinView;
     private Button btnVerif, btnResend;
+    private String OTP;
+    private FirebaseAuth firebaseAuth;
+    private Context context;
+    private SharedPreferences preferences;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private String phoneNumber;
+    private String name;
+    private String email;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,11 +56,19 @@ public class OTPGenerate extends AppCompatActivity {
     }
 
     public void inisialisasi() {
+        preferences = OTPGenerate.this.getSharedPreferences(getString(R.string.preference_key), MODE_PRIVATE);
         pinView = findViewById(R.id.firstPinView);
         pinView.setTextColor(
                 ResourcesCompat.getColorStateList(getResources(), R.color.newBlue, getTheme()));
         pinView.setLineColor(
                 ResourcesCompat.getColorStateList(getResources(), R.color.newBlue, getTheme()));
+
+        OTP = getIntent().getStringExtra("auth");
+        phoneNumber = getIntent().getStringExtra("phoneNumber");
+        name = getIntent().getStringExtra("name");
+        email = getIntent().getStringExtra("email");
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         pinView.setItemCount(6);
         pinView.setItemHeight(getResources().getDimensionPixelSize(R.dimen.pv_pin_view_item_size));
@@ -60,7 +95,13 @@ public class OTPGenerate extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // check apakah betul, kalau betul
-                moveToFragment();
+                String verifCode = pinView.getText().toString();
+                if(!verifCode.isEmpty()){
+                    PhoneAuthCredential credential = PhoneAuthProvider.getCredential(OTP, verifCode);
+                    signIn(credential);
+                }else{
+                    Toast.makeText(OTPGenerate.this, "Tolong masukkan kode OTP", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -69,6 +110,45 @@ public class OTPGenerate extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // buat ulang
+            }
+        });
+    }
+
+    private void signIn(final PhoneAuthCredential credential){
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    db.collection("Users").document(phoneNumber).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                        @Override
+                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                            if(!documentSnapshot.exists()){
+                                User newUser = new User(name, email, phoneNumber,null);
+                                Map<String, Object> res = new HashMap<>();
+                                res.put("id",newUser.getUserId());
+                                res.put("name",newUser.getUserName());
+                                res.put("email", newUser.getUserEmail());
+                                res.put("image", newUser.toString());
+                                res.put("phoneNumber",newUser.getUserTelephoneNumber());
+                                db.collection("Users").document(phoneNumber).set(newUser);
+                                Constant.currentUser = newUser;
+                            } else {
+                                User user = new User(documentSnapshot.getString("id"),
+                                        documentSnapshot.getString("name"),
+                                        documentSnapshot.getString("email"),
+                                        documentSnapshot.getString("phoneNumber"),
+                                        Uri.parse(documentSnapshot.getString("image")));
+                                Constant.currentUser = user;
+                            }
+                        }
+                    });
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("credentials",credential.toString());
+                    editor.apply();
+                    moveToFragment();
+                }else{
+                    Toast.makeText(OTPGenerate.this, "Kode OTP Salah", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
--- app/src/main/java/com/u_binusportal/activity/entriesActivities/entries/RegisterActivity.java
+++ app/src/main/java/com/u_binusportal/activity/entriesActivities/entries/RegisterActivity.java
@@ -2,16 +2,32 @@ package com.u_binusportal.activity.entriesActivities.entries;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.u_binusportal.Constant;
 import com.u_binusportal.OTPGenerate;
 import com.u_binusportal.forTesting.DatabaseTest;
 import com.u_binusportal.R;
@@ -20,14 +36,25 @@ import com.u_binusportal.forTesting.UserTesting;
 import com.u_binusportal.handlers.FragmentHandler;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText NameField;
     private EditText PhoneNumberField;
     private EditText EmailField;
+    private String email;
+    private String name;
+    private String phonenumber;
 
     private Button RegisterButton;
     private ProgressDialog Progress;
+    private FirebaseAuth firebaseAuth;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks callbacks;
+    private SharedPreferences preferences;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public ArrayList<User> users = new ArrayList<User>();
 
@@ -35,6 +62,8 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar_user);
+        firebaseAuth = FirebaseAuth.getInstance();
+        preferences = RegisterActivity.this.getSharedPreferences(getString(R.string.preference_key), MODE_PRIVATE);
 
         NameField = findViewById(R.id.input_nama_daftar_user);
         PhoneNumberField = findViewById(R.id.input_phone_number_daftar_user);
@@ -53,9 +82,9 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void startRegisterUser() {
-        String email = EmailField.getText().toString().trim();
-        String name = NameField.getText().toString().trim();
-        String phonenumber = PhoneNumberField.getText().toString().trim();
+        email = EmailField.getText().toString().trim();
+        name = NameField.getText().toString().trim();
+        phonenumber = PhoneNumberField.getText().toString().trim().replaceFirst("0","+62");
 
         if (TextUtils.isEmpty(name)) {
             NameField.requestFocus();
@@ -73,19 +102,79 @@ public class RegisterActivity extends AppCompatActivity {
             PhoneNumberField.requestFocus();
             PhoneNumberField.setError("Nomor telepon harus diantara 11-13 digit");
         } else {
+            db.collection("Users").document(phonenumber).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    if(documentSnapshot.exists()){
+                        Toast.makeText(RegisterActivity.this, "User telah didaftarkan. Silahkan masuk", Toast.LENGTH_SHORT).show();
+                    }else{
+                        callbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                            @Override
+                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                signIn(phoneAuthCredential);
+                            }
 
-//                Progress.setMessage("Signing up ...");
-//                Progress.show();
-                User newUser = new User(name,email,phonenumber,null);
-                DatabaseTest.addUser(newUser);
-//                Progress.dismiss();
-                Toast.makeText(RegisterActivity.this,"Register Success", Toast.LENGTH_LONG).show();
-                UserTesting.hasUser = true;
+                            @Override
+                            public void onVerificationFailed(@NonNull FirebaseException e) {
+                                Toast.makeText(RegisterActivity.this, "Error!" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
 
-                // INI INTENTNYA UNTUK KE OTP
-                startActivity(new Intent(this, OTPGenerate.class));
+                            @Override
+                            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                super.onCodeSent(s, forceResendingToken);
+                                Toast.makeText(RegisterActivity.this, "Kode OTP telah dikirimkan", Toast.LENGTH_SHORT).show();
+                                Intent otpIntent = new Intent(RegisterActivity.this, OTPGenerate.class);
+                                otpIntent.putExtra("auth", s);
+                                otpIntent.putExtra("name", name);
+                                otpIntent.putExtra("email",email);
+                                otpIntent.putExtra("phoneNumber", phonenumber);
+                                startActivity(otpIntent);
+                            }
+                        };
+
+                        PhoneAuthOptions options =
+                                PhoneAuthOptions.newBuilder(firebaseAuth)
+                                        .setPhoneNumber(phonenumber)
+                                        .setTimeout(60L, TimeUnit.SECONDS)
+                                        .setActivity(RegisterActivity.this)
+                                        .setCallbacks(callbacks)
+                                        .build();
+                        PhoneAuthProvider.verifyPhoneNumber(options);
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(RegisterActivity.this, "Error!" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
             }
+        }
 
+        private void signIn(final PhoneAuthCredential credential){
+            firebaseAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        User newUser = new User(name, email, phonenumber,null);
+                        Map<String, Object> res = new HashMap<>();
+                        res.put("id",newUser.getUserId());
+                        res.put("name",newUser.getUserName());
+                        res.put("email", newUser.getUserEmail());
+                        res.put("image", newUser.toString());
+                        res.put("phoneNumber",newUser.getUserTelephoneNumber());
+                        db.collection("Users").document(phonenumber).set(newUser);
+                        Constant.currentUser = newUser;
+                        SharedPreferences.Editor editor = preferences.edit();
+                        editor.apply();
+                        Toast.makeText(RegisterActivity.this, "Sukses Masuk!", Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(RegisterActivity.this, FragmentHandler.class);
+                        startActivity(i);
+                    }else{
+                        Toast.makeText(RegisterActivity.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         }
 }
 
